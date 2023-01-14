aafba6f1502717c01c102d2a55f218ec1ff92575
renatillas <renata.amutio@gmail.com>
2023-01-14 23:31:26 +0100

    Anyadidas clases al commit anterior que me habia olvidado

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+
+    private Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPosition;
+    }
+}
