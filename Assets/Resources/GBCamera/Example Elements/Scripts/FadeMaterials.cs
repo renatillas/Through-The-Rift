2a47461263bd326760ff4140953d0b6f99d6f9bd
renatillas <renata.amutio@gmail.com>
2023-01-16 04:14:30 +0100

    Cambie las cosas de lugar. Anadi hit effects y death effects.

---
+++ Assets/Resources/GBCamera/Example Elements/Scripts/FadeMaterials.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Camara_GBA.GBCamera.Example_Elements.Scripts
+{
+    public class FadeMaterials : MonoBehaviour
+    {
+        [FormerlySerializedAs("_materials")] public Material[] materials;
+
+        [FormerlySerializedAs("_fadeAmountPerSecond")]
+        public float fadeAmountPerSecond;
+
+
+        public IEnumerator FadeIn()
+        {
+            var complete = false;
+
+            while (!complete)
+            {
+                complete = true;
+
+                for (var i = 0; i < materials.Length; i++)
+                {
+                    var fade = materials[i].GetFloat("_Fade");
+
+                    fade = Mathf.Min(1f, fade + (fadeAmountPerSecond * Time.deltaTime));
+                    if (fade < 1f)
+                    {
+                        complete = false;
+                    }
+
+                    materials[i].SetFloat("_Fade", fade);
+                }
+
+                yield return null;
+            }
+
+            yield break;
+        }
+
+        public IEnumerator FadeOut()
+        {
+            var complete = false;
+
+            while (!complete)
+            {
+                complete = true;
+
+                for (var i = 0; i < materials.Length; i++)
+                {
+                    var fade = materials[i].GetFloat("_Fade");
+
+                    fade = Mathf.Max(0f, fade - (fadeAmountPerSecond * Time.deltaTime));
+                    if (fade > 0)
+                    {
+                        complete = false;
+                    }
+
+                    materials[i].SetFloat("_Fade", fade);
+                }
+
+                yield return null;
+            }
+
+            yield break;
+        }
+    }
+}
